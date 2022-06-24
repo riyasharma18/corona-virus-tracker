c20a02e7b5e09b9750ebf02df6aa47c36496fc66
riyasharma18 <sharmariyasharma1039@gmail.com>
2022-06-24 17:41:49 +0530

    Updates

---
+++ precache-manifest.58b6316fcfb27a096ff33b3edd217aa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de85d195dd087ebe1dc3b1e4093394b",
+    "url": "/corona-virus-tracker/index.html"
+  },
+  {
+    "revision": "9d5e67f6bdf759382241",
+    "url": "/corona-virus-tracker/static/css/main.32c307ff.chunk.css"
+  },
+  {
+    "revision": "17f7b3b1f7c221b4d42e",
+    "url": "/corona-virus-tracker/static/js/2.d535fbb7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-virus-tracker/static/js/2.d535fbb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5e67f6bdf759382241",
+    "url": "/corona-virus-tracker/static/js/main.9043b10a.chunk.js"
+  },
+  {
+    "revision": "cb7c1121b685c8430700",
+    "url": "/corona-virus-tracker/static/js/runtime-main.e4aeccf1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-virus-tracker/static/media/image.d7265326.png"
+  }
+]);
